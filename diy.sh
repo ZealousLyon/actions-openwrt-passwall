0edfc8261baa691a7ba9961015059d64824ac0cf
Zea1ous <304117651@qq.com>
2020-02-29 15:22:32 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
-git clone https://github.com/rosywrt/luci-theme-rosy
+# sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+# git clone https://github.com/rosywrt/luci-theme-rosy
